3f5177db953abbeb7e1b3c7c7f5d998a66a33166
Ricardo Gabriel <riccardoamalves@gmail.com>
2022-12-31 09:44:51 -0300

    commit inicial

---
+++ MEUSITE.py
@@ -0,0 +1,6 @@
+nome=input("Whats your name ?")
+idade=input("Whats your age ?")
+peso=input("Whats your weight")
+email=input("Whats your email,please ?")
+print("Nice to meet you",nome,"!")
+print("Do you fell good about your current weight ?")
