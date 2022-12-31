0d9b3d4e69f68c2fc702b5ef189381399d0127f1
Ricardo Gabriel <riccardoamalves@gmail.com>
2022-12-31 09:51:58 -0300

    v1

--- SITE.py
+++
@@ -1,6 +0,0 @@
-nome=input("Whats your name ?")
-idade=input("Whats your age ?")
-peso=input("Whats your weight")
-email=input("Whats your email,please ?")
-print("Nice to meet you",nome,"!")
-print("Do you fell good about your current weight ?")
